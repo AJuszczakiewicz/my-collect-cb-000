b61abb7351a7ebb43c5e4c0bda0364c6df5af162
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-11 15:20:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    while i < array.size
+      new_array << yield(array[i])
+      i += 1
+    end
+    new_array
+  else
+    array
+  end
+end
 
