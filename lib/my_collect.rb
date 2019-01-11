0d49aa55cae9b0b6cf1d3422c78daf8304ad2fb1
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-11 15:20:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,12 @@
 def my_collect(array)
   if block_given?
     i = 0
+    collection = Array.new
     while i < array.size
-      new_array << yield(array[i])
+      collection << yield(array[i])
       i += 1
     end
-    new_array
+    collection
   else
     array
   end
